396c33f26c5c9e1f57ea7ec94d9685fe909b4bb0
Jeffrey Bahr <bahr.programmer@gmail.com>
2022-08-19 13:46:44 -0600

    Binary Nary Node classes

---
+++ BinaryNode.cs
@@ -0,0 +1,31 @@
+﻿public class BinaryNode<T>
+{
+    public T Value { get; private set; }
+    public BinaryNode<T>? LeftChild { get; set; }
+    public BinaryNode<T>? RightChild { get; set; }
+
+    public BinaryNode(T t)
+    {
+        Value = t;
+        LeftChild = null;
+        RightChild = null;
+    }
+
+    public void AddLeft(BinaryNode<T>? child)
+    {
+        LeftChild = child;
+    }
+
+    public void AddRight(BinaryNode<T>? child)
+    {
+        RightChild = child;
+    }
+
+    public override string ToString()
+    {
+        var leftChildValue = LeftChild != null ? LeftChild.Value?.ToString() : "null";
+        var rightChildValue = RightChild != null ? RightChild.Value?.ToString() : "null";
+
+        return $"{Value}: {leftChildValue} {rightChildValue}";
+    }
+}
---
+++ NaryNode.cs
@@ -0,0 +1,21 @@
+﻿public class NaryNode<T>
+{
+    public T Value { get; private set; }
+    public List<NaryNode<T>> Children { get; private set; }
+
+    public NaryNode(T t)
+    {
+        Value = t;
+        Children = new List<NaryNode<T>>();
+    }
+
+    public void AddChild(NaryNode<T> child)
+    {
+        Children.Add(child);
+    }
+
+    public override string ToString()
+    {
+        return $"{Value}: {Children.Aggregate("", (current, child) => $"{current} {child.Value?.ToString()}")}";
+    }
+}
